ad5e23c3e5e011e1b324fc045f4b51c2ce752038
sysaravi <sysaravi.xworkz@gmail.com>
2021-10-26 10:17:20 +0530

    java files

---
+++ variables/bgmit.java
@@ -0,0 +1,5 @@
+class bgmit{
+public static void main(String args[]){
+System.out.println("hi hello bgmit");
+}
+}
---
+++ variables/keyword.java
@@ -0,0 +1,5 @@
+class keyword{
+public static void main(String args[]){
+        System.out.println("num of keyword");
+    }
+}
