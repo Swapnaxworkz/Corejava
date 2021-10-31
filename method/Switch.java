44822ed25017c26610349d66ca7ff6c2bdea666a
sysaravi <sysaravi.xworkz@gmail.com>
2021-10-31 12:18:00 +0530

    java files

---
+++ method/Switch.java
@@ -0,0 +1,24 @@
+ class Switch
+ {
+  void board(String types, int voltage)
+  {
+    System.out.println("Switch name is "+types);
+    System.out.println("Switch volt is "+voltage);
+  }
+  void board(int num, int voltage)
+  {
+    System.out.println("Switch board is "+num);
+    System.out.println("Switch board is "+voltage);
+  }
+  void buttons(float num) 
+ {
+  System.out.println(" buttons "+num);
+	}
+   void buttons(double numbers) {
+   
+    System.out.println("buttons"+numbers);
+ 
+  }
+  }
+  
+  
