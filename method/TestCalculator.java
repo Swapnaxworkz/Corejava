bc337d85489e117e5c44b161dc5779b19ede9345
Swapnaxworkz <93175744+Swapnaxworkz@users.noreply.github.com>
2021-10-31 21:27:36 +0530

    Delete TestCalculator.java

--- method/TestCalculator.java
+++
@@ -1,9 +0,0 @@
-class TestCalculator 
-{
-public static void main(String []args)
-{
-System.out.println(Calculator.add(3,5,7,5));
-System.out.println(Calculator. sub(4,8,9));
-System.out.println(Calculator.mul(8,9,6));
-}
-}
