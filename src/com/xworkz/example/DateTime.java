d94b15c49288c0d225292ade7be92bf15ced4839
sysaravi <sysaravi.xworkz@gmail.com>
2022-01-03 10:45:29 +0530

    example

---
+++ src/com/xworkz/example/DateTime.java
@@ -0,0 +1,13 @@
+package com.xworkz.example;
+
+import java.time.LocalDateTime;
+
+public class DateTime {
+	
+			public static void main(String[] args) {
+				LocalDateTime datetime = LocalDateTime.now();
+				System.out.println(datetime);	
+			}
+		}
+
+
---
+++ src/com/xworkz/example/MathOperator.java
@@ -0,0 +1,12 @@
+package com.xworkz.example;
+
+public class MathOperator {
+
+		public static void main(String[] args) {
+			System.out.println(Math.sqrt(22));
+			System.out.println(Math.min(23,456));
+			System.out.println(Math.max(1275,87536));
+		}
+
+	}
+
